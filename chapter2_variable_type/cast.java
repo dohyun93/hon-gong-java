3568ce94f2828f80d8d217f598b062124c395fab
dohyun93 <kwondohyun93@naver.com>
2024-09-18 15:19:24 +0900

    commit 0918

--- chapter2_variable_type/cast.java
+++ chapter2_variable_type/cast.java
@@ -1,15 +1,17 @@
 package chapter2_variable_type;
 
+import javax.swing.*;
+
 public class cast{
     public static void main(String[] args) {
         // 문자열을 실수로 변경 : Double.parseDouble(문자열)
-        String var1 = "1.23";
-        double var1Int = Double.parseDouble(var1);
-        System.out.println(var1Int);
+        String stringDouble = "1.23";
+        double parsedDouble = Double.parseDouble(stringDouble);
+        System.out.println(parsedDouble);
 
         // 정수를 문자열로 변경 : String.valueOf(정수)
         int myInt = 999;
-        String intToString = String.valueOf(myInt);
-
+        String stringfiedInt = String.valueOf(myInt);
+        System.out.println(stringfiedInt);
     }
 }
